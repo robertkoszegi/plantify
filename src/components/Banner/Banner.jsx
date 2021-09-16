9452fe0e3106eaa876f4fed926b30cc1a7397bc5
Sean <sean.rockwood@gmail.com>
2021-09-16 19:45:55 -0400

    Updated base component to run, nav, banner, content out of state

---
+++ src/components/Banner/Banner.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+function Banner(props) {
+    return (
+      <div className="Banner">
+        <h1>Banner</h1>
+        <p>Take the plant quiz!!</p>
+      </div>
+    );
+  }
+  
+  // Must export the component's function (or class)
+  export default Banner;
+  
