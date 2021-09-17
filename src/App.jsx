1ffa548db3c8ca67c5419d3a4d41141f46190897
robertkoszegi <robkoszegi@gmail.com>
2021-09-17 17:11:21 -0400

    merge fixes

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React, {Component} from 'react';
-import { Route, Switch, Redirect } from 'react-router-dom';
+import { Route, Switch } from 'react-router-dom';
 // import logo from './logo.svg';
 import Navigation from './components/Navigation/Navigation'
 import Banner from './components/Banner/Banner'
