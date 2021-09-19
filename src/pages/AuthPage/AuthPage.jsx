2f6fb58f4f62a289f836569c743e31dd27932e53
robertkoszegi <robkoszegi@gmail.com>
2021-09-19 19:10:25 -0400

    Login components added

---
+++ src/pages/AuthPage/AuthPage.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import Sin from '../../components/Navigation/Navigation';
+
+
+class WishListPage extends React.Component {
+    state = {
+
+    }
+
+
+    render() {
+        return(
+            <main className="WishListPage">
+                
+                <h1>Authentication</h1>
+            </main>
+        )
+    }
+
+}
